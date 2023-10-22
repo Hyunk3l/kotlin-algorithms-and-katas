258e5f4e091b6ceb9385f2879ef472dc540daafc
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-10-22 22:43:52 +0200

    Add more use cases

--- src/main/kotlin/LambdasInKotlin.kt
+++ src/main/kotlin/LambdasInKotlin.kt
@@ -3,9 +3,19 @@ private fun operation(first: Int, second: Int, predicate: (Int, Int) -> Int) {
     println("Operation $first $second: ${predicate(first, second)}")
 }
 
+private fun <T> genericOperation(first: T, second: T, predicate: (T, T) -> T) {
+    println("Generic operation $first $second: ${predicate(first, second)}")
+}
+
 fun main() {
     operation(2, 2) { a, b -> a + b }
     operation(2, 3) { a, b -> a * b }
     operation(6, 2) { a, b -> a - b }
     operation(6, 2) { a, b -> a / b }
+
+    println("--------------------------------------")
+
+    genericOperation(2, 2) { a, b -> a + b }
+    genericOperation("Hello", "World") { a, b -> "$a $b" }
+    genericOperation("Hello", 2) { a, b -> "$a + $b" }
 }
