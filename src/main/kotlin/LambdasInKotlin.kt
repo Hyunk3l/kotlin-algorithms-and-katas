f62cbc5e37d0c78a62c9a83da87276768aa4faab
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-10-22 22:20:50 +0200

    Add Lambdas

---
+++ src/main/kotlin/LambdasInKotlin.kt
@@ -0,0 +1,11 @@
+
+private fun operation(first: Int, second: Int, predicate: (Int, Int) -> Int): Unit {
+    println("Operation $first $second: ${predicate(first, second)}")
+}
+
+fun main() {
+    operation(2, 2) { a, b -> a + b }
+    operation(2, 3) { a, b -> a * b }
+    operation(6, 2) { a, b -> a - b }
+    operation(6, 2) { a, b -> a / b }
+}
