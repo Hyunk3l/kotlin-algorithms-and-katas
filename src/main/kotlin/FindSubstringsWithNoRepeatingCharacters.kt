892d15169a41df284e66e492e1976141b6e5274f
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-10-22 22:46:56 +0200

    Add more use cases

---
+++ src/main/kotlin/FindSubstringsWithNoRepeatingCharacters.kt
@@ -0,0 +1,22 @@
+package com.fabridinapoli.kotlinalgorithms
+
+class FindSubstringsWithNoRepeatingCharacters {
+    fun findFrom(initial: String): List<String> {
+        val combinations = mutableListOf<String>()
+        val characters = initial.toCharArray()
+        for (element in characters) {
+            var accumulated = element.toString()
+            for (secondElement in characters) {
+                if (
+                    !combinations.contains(element.toString())
+                    && accumulated.contains(secondElement)
+                    && element != secondElement
+                ) {
+                    accumulated = "${accumulated}${secondElement}"
+                }
+            }
+            combinations.add(accumulated)
+        }
+        return combinations
+    }
+}
