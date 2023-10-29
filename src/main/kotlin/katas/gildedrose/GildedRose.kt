48fef3a318fdcf2c5f270f7c48e5d65176aef3b7
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-10-29 19:42:34 +0100

    Constants

--- src/main/kotlin/katas/gildedrose/GildedRose.kt
+++ src/main/kotlin/katas/gildedrose/GildedRose.kt
@@ -1,15 +1,19 @@
 package katas.gildedrose
 
 private const val MAX_QUALITY = 50
+private const val AGED_BRIE = "Aged Brie"
+private const val BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+
+private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
 
 class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
 
         items.forEach {
-            if (it.name != "Aged Brie" && it.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (it.name != AGED_BRIE && it.name != BACKSTAGE) {
                 if (it.quality > 0) {
-                    if (it.name != "Sulfuras, Hand of Ragnaros") {
+                    if (it.name != SULFURAS) {
                         it.quality -= 1
                     }
                 }
@@ -17,7 +21,7 @@ class GildedRose(var items: List<Item>) {
                 if (it.quality < MAX_QUALITY) {
                     it.quality += 1
 
-                    if (it.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (it.name == BACKSTAGE) {
                         if (it.sellIn < 11) {
                             if (it.quality < MAX_QUALITY) {
                                 it.quality += 1
@@ -33,15 +37,15 @@ class GildedRose(var items: List<Item>) {
                 }
             }
 
-            if (it.name != "Sulfuras, Hand of Ragnaros") {
+            if (it.name != SULFURAS) {
                 it.sellIn -= 1
             }
 
             if (it.sellIn < 0) {
-                if (it.name != "Aged Brie") {
-                    if (it.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (it.name != AGED_BRIE) {
+                    if (it.name != BACKSTAGE) {
                         if (it.quality > 0) {
-                            if (it.name != "Sulfuras, Hand of Ragnaros") {
+                            if (it.name != SULFURAS) {
                                 it.quality -= 1
                             }
                         }
