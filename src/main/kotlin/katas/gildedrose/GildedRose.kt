380089050da1c336b93df8c37afcfec924df64cb
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-10-29 20:49:55 +0100

    Cleaned up

--- src/main/kotlin/katas/gildedrose/GildedRose.kt
+++ src/main/kotlin/katas/gildedrose/GildedRose.kt
@@ -1,13 +1,5 @@
 package katas.gildedrose
 
-private const val MAX_QUALITY = 50
-private const val AGED_BRIE = "Aged Brie"
-private const val BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
-private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
-
 class GildedRose(var items: List<BaseItem>) {
-
-    fun updateQuality() {
-        items.forEach { it.update() }
-    }
+    fun updateQuality() { items.forEach { it.update() } }
 }
