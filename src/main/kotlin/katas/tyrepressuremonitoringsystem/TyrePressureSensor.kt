1f591f35e7a2d28ae87ee6eb5f2e85693d90ae43
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-11-01 15:03:06 +0100

    Refactor

--- src/main/kotlin/katas/tyrepressuremonitoringsystem/TyrePressureSensor.kt
+++ src/main/kotlin/katas/tyrepressuremonitoringsystem/TyrePressureSensor.kt
@@ -4,15 +4,10 @@ import java.util.Random
 
 private const val OFFSET = 16.0
 
+private const val MULTIPLIER = 6.0
+
 class TyrePressureSensor(private val random: Random) : Sensor {
-    override fun popNextValue(): Double {
-        val pressureTelemetryValue = samplePressure()
+    override fun popNextValue(): Double = OFFSET + samplePressure()
 
-        return OFFSET + pressureTelemetryValue
-    }
-
-    private fun samplePressure(): Double {
-        // placeholder implementation that simulate a real sensor in a real tire
-        return 6.0 * random.nextDouble() * random.nextDouble()
-    }
+    private fun samplePressure() = MULTIPLIER * random.nextDouble() * random.nextDouble()
 }
