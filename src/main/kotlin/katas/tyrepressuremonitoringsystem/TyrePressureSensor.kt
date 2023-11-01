173f6e262b54a19e1bf202d41580807cecac3027
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-11-01 15:03:12 +0100

    Refactor

--- src/main/kotlin/katas/tyrepressuremonitoringsystem/TyrePressureSensor.kt
+++ src/main/kotlin/katas/tyrepressuremonitoringsystem/TyrePressureSensor.kt
@@ -3,7 +3,6 @@ package katas.tyrepressuremonitoringsystem
 import java.util.Random
 
 private const val OFFSET = 16.0
-
 private const val MULTIPLIER = 6.0
 
 class TyrePressureSensor(private val random: Random) : Sensor {
