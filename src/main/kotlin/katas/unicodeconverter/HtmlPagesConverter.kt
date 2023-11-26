97e273a3d69af6691c1573be0bc179944358956f
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2023-11-26 09:40:16 +0100

    Refactor

--- src/main/kotlin/katas/unicodeconverter/HtmlPagesConverter.kt
+++ src/main/kotlin/katas/unicodeconverter/HtmlPagesConverter.kt
@@ -4,27 +4,27 @@ import java.io.BufferedReader
 import java.io.FileReader
 import java.io.IOException
 
+private const val PAGE_BREAK = "PAGE_BREAK"
+
 class HtmlPagesConverter(filename: String) {
     private val content = HashMap<Int, List<String>>()
 
     init {
-        val reader = try {
-            BufferedReader(FileReader(filename))
+        try {
+            BufferedReader(FileReader(filename)).use { reader ->
+                var currentPage = 0
+                val lines = reader.readLines()
+                lines.forEach { line ->
+                    if (line == PAGE_BREAK) {
+                        currentPage++
+                    } else {
+                        content[currentPage] = content.getOrElse(currentPage) { emptyList() } + line
+                    }
+                }
+            }
         } catch (exception: IOException) {
             throw ReadingError(exception.message)
         }
-
-        var currentPage = 0
-        var line: String? = reader.readLine()
-        while (line != null) {
-            if (line == "PAGE_BREAK") {
-                currentPage++
-            } else {
-                content[currentPage] = content.getOrDefault(currentPage, emptyList()) + line
-            }
-            line = reader.readLine()
-        }
-        reader.close()
     }
 
     fun getHtmlPage(page: Int): String {
@@ -33,7 +33,7 @@ class HtmlPagesConverter(filename: String) {
         val htmlPage = StringBuffer()
 
         pageContent.forEach { singleLine ->
-            if (!singleLine.contains("PAGE_BREAK")) {
+            if (!singleLine.contains(PAGE_BREAK)) {
                 htmlPage.append(StringEscapeUtils.escapeHtml(singleLine))
                 htmlPage.append("<br />")
             }
