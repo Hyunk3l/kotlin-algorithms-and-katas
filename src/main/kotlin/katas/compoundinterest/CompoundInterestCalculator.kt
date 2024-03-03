335518a1bf2af325670437a9ed56347b92726408
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2024-03-03 19:04:33 +0100

    Add logic

--- src/main/kotlin/katas/compoundinterest/CompoundInterestCalculator.kt
+++ src/main/kotlin/katas/compoundinterest/CompoundInterestCalculator.kt
@@ -1,8 +1,18 @@
 package katas.compoundinterest
 
+import kotlin.math.pow
+
 class CompoundInterestCalculator {
-    fun calculate(parameters: CompoundInterestParameters) {
-        TODO()
+    fun calculate(parameters: CompoundInterestParameters): Double {
+        val power = (parameters.lengthInYears * 12).toDouble()
+        val interestRate = parameters.estimatedInterestRate.toDouble()/100.0
+        val base = 1 + interestRate/12.0
+        val formula = base.pow(power)
+
+        val initial = parameters.initialInvestment * formula
+        val monthly = (parameters.monthlyContribution * (formula - 1))/(interestRate/12)
+
+        return initial + monthly
     }
 }
 
