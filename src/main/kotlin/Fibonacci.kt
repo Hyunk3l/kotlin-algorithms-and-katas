fd1eafc0e5bdd8047bd0f564038defdd6a535f6c
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2024-06-09 00:10:34 +0200

    Add fibonacci using brute force

---
+++ src/main/kotlin/Fibonacci.kt
@@ -0,0 +1,14 @@
+
+
+class Fibonacci {
+    fun getFrom(number: Int): Int {
+        if (number <= 1) { return number }
+        val counter = mutableMapOf<Int, Int>()
+        counter[0] = 0
+        counter[1] = 1
+        for (i in 2..number) {
+            counter[i] = counter[i-2]!! + counter[i-1]!!
+        }
+        return counter[number]!!
+    }
+}
