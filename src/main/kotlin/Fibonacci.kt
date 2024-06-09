1795c230dd9d471eab077b3ae47841b6bdbc213e
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2024-06-09 09:07:02 +0200

    Add more cases

--- src/main/kotlin/Fibonacci.kt
+++ src/main/kotlin/Fibonacci.kt
@@ -1,14 +1,37 @@
-
-
 class Fibonacci {
-    fun getFromUsingBruteForce(number: Int): Int {
-        if (number <= 1) { return number }
+    /**
+     * Iterative DP (Tabulation) - time complexity of O(n) and space complexity of O(n)
+     */
+    fun getFromUsingIterativeDynamicProgramming(number: Int): Int {
+        if (number <= 1) {
+            return number
+        }
         val counter = mutableMapOf<Int, Int>()
         counter[0] = 0
         counter[1] = 1
         for (i in 2..number) {
-            counter[i] = counter[i-2]!! + counter[i-1]!!
+            counter[i] = counter[i - 2]!! + counter[i - 1]!!
         }
         return counter[number]!!
+    }
+
+    /**
+     * Not efficient - Brute force and recursive. Exponential time complexity.
+     */
+    fun getFromRecursive(number: Int): Int {
+        if (number <= 1) return number
+        return getFromRecursive(number - 1) + getFromRecursive(number - 2)
+    }
+
+    /**
+     * Memoization (Recursive Dynamic Programming).
+     */
+    fun getFromMemoization(number: Int, memo: MutableMap<Int, Int>): Int {
+        println("$number ${memo.values}")
+        if (memo.containsKey(number)) return memo[number]!!
+        if (number <= 1) return number
+
+        memo[number] = getFromMemoization(number - 1, memo) + getFromMemoization(number - 2, memo)
+        return memo[number]!!
     }
 }
--- src/test/kotlin/FibonacciTest.kt
+++ src/test/kotlin/FibonacciTest.kt
@@ -3,14 +3,28 @@ import org.junit.jupiter.api.Test
 
 class FibonacciTest {
     @Test
-    fun `should solve simple fibonacci of 1 using brute force`() {
-        val result = Fibonacci().getFromUsingBruteForce(1)
+    fun `should solve simple fibonacci of 1 using iterative DP (tabulation)`() {
+        val result = Fibonacci().getFromUsingIterativeDynamicProgramming(1)
 
         result shouldBe 1
     }
     @Test
-    fun `should solve fibonacci of 10 using brute force`() {
-        val result = Fibonacci().getFromUsingBruteForce(10)
+    fun `should solve fibonacci of 10 using iterative DP (tabulation)`() {
+        val result = Fibonacci().getFromUsingIterativeDynamicProgramming(10)
+
+        result shouldBe 55
+    }
+
+    @Test
+    fun `should solve fibonacci of 10 using recursive (brute force simple rec)`() {
+        val result = Fibonacci().getFromRecursive(10)
+
+        result shouldBe 55
+    }
+
+    @Test
+    fun `should solve fibonacci of 10 using memoization (recursive DP)`() {
+        val result = Fibonacci().getFromMemoization(10, mutableMapOf())
 
         result shouldBe 55
     }
