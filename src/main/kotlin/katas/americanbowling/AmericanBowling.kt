3fddf9bbf123d99f7b54d23bd71c81951173906d
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2024-08-11 20:24:28 +0200

    Add test case

--- src/main/kotlin/katas/americanbowling/AmericanBowling.kt
+++ src/main/kotlin/katas/americanbowling/AmericanBowling.kt
@@ -4,6 +4,14 @@ class AmericanBowling {
     fun calculate(score: String): Int {
         val points = score.split(" ")
 
-        return points.map { if (it == "X") 25 else 0 }.sum()
+        return points
+            .map {
+                if (it == "X") {
+                    25
+                } else if (it == "9-") {
+                    9
+                } else 0
+            }
+            .sum()
     }
 }
--- src/test/kotlin/katas/americanbowling/AmericanBowlingShould.kt
+++ src/test/kotlin/katas/americanbowling/AmericanBowlingShould.kt
@@ -12,4 +12,13 @@ class AmericanBowlingShould {
 
         result shouldBe 300
     }
+
+    @Test
+    fun `calculate 20 rolls, 10 pairs of 9 and miss`() {
+        val score = "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-"
+
+        val result = AmericanBowling().calculate(score)
+
+        result shouldBe 90
+    }
 }
