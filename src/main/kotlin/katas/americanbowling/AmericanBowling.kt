c617507e7808357692e097ca0ee27a3c3f0c53ff
Fabrizio Di Napoli <fabri.dinapoli@gmail.com>
2025-07-12 20:34:27 +0200

    Add unfinished bowling

--- src/main/kotlin/katas/americanbowling/AmericanBowling.kt
+++ src/main/kotlin/katas/americanbowling/AmericanBowling.kt
@@ -14,16 +14,29 @@ class AmericanBowling {
 
         return points
             .map {
-                if (it.isStrike()) {
+                val shots = it.split("")
+
+                if (shots[1].isStrike() || shots.isSpare()) {
                     10
-                } else if (it == "9-") {
+                } else if (shots[1] == "9" && shots[2] == "-") {
                     9
+                } else if (shots[2].isEmpty()) {
+                    throw RuntimeException("Game not finished, player is still missing one shot")
+                } else if (shots.isSummable()) {
+                    shots[1].toInt() + shots[2].toInt()
                 } else 0
             }
             .sum()
     }
 
     private fun String.isStrike() = this == STRIKE
+
+    private fun List<String>.isSpare() = this[2] == "/"
+
+    private fun List<String>.isSummable(): Boolean {
+        return this[1].toCharArray()[0].isDigit()
+                && this[2].toCharArray()[0].isDigit()
+    }
 
     private fun List<String>.isAllStrikes(): Boolean {
         if (this.size != MAX_FRAMES_WITH_STRIKES) return false
--- src/test/kotlin/katas/americanbowling/AmericanBowlingShould.kt
+++ src/test/kotlin/katas/americanbowling/AmericanBowlingShould.kt
@@ -1,6 +1,9 @@
 package katas.americanbowling
 
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.startWith
 import org.junit.jupiter.api.Test
 
 class AmericanBowlingShould {
@@ -29,5 +32,42 @@ class AmericanBowlingShould {
         val result = AmericanBowling().calculate(score)
 
         result shouldBe 91
+    }
+
+    @Test
+    fun `calculate 19 rolls, 1 strike, 1 spare, 8 pairs of 9 and miss`() {
+        val score = "X 5/ 9- 9- 9- 9- 9- 9- 9- 9-"
+
+        val result = AmericanBowling().calculate(score)
+
+        result shouldBe 92
+    }
+
+    @Test
+    fun `calculate 19 rolls, 1 strike, 1 spare, 1 frame of 4, 7 pairs of 9 and miss`() {
+        val score = "X 5/ 9- 9- 9- 9- 9- 9- 9- 31"
+
+        val result = AmericanBowling().calculate(score)
+
+        result shouldBe 87
+    }
+
+    @Test
+    fun `calculate 9 strikes and one missed frame`() {
+        val score = "X X X X X X X X X --"
+
+        val result = AmericanBowling().calculate(score)
+
+        result shouldBe 240
+    }
+
+    @Test
+    fun `throw an error in case game is not finished`() {
+        val score = "X 9- 9- 9- 9- 9- 9- 9- 9- 9"
+
+        val exception = shouldThrow<RuntimeException> {
+            AmericanBowling().calculate(score)
+        }
+        exception.message should startWith("Game not finished, player is still missing one shot")
     }
 }
